0840217f291dc7d3655e78a1ddace7aacf52a83b
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-23 15:47:40 +0300

    completed my assigment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+returnFirstTwoDrivers()
+const returnFirstTwoDrivers
+const returnLastTwoDrivers()
+selectingDrivers
+returnFirstTwoDrivers()
+returnLastTwoDrivers()
+createFareMultiplier(4)
+fareDoubler()
+const createFareMultiplier()
+createFareMultiplier()
+fareDoubler()
+fareTripler()
+const cFareMultiplier()
+createFareMultiplier()
+fareTripler()
+selectDifferentDrivers() 
+drivers
+returnFirstTwoDrivers()
+returnLastTwoDrivers()
+selectDifferentDrivers()
